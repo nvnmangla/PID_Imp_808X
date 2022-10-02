8ebdcde184d16dd65eb2d7357adbf36ac8045e02
Naveen <nmangla@umd.edu>
2022-10-02 17:35:51 -0400

    Test case update

--- test/test.cpp
+++ test/test.cpp
@@ -1,7 +1,15 @@
 #include <gtest/gtest.h>
 #include "pid.hpp"
 
+
+// Test Case 1
 TEST(PID, output) {
   PID pid(0.01, 0.1, 0.5, 100, -100, 0.1);
   EXPECT_EQ(2.5, pid.cal_pid(20,0));
+}
+
+// Test Case 2
+TEST(PID, output) {
+  PID pid(0.01, 0.1, 0.5, 100, -100, 0.1);
+  EXPECT_EQ(2.5, pid.cal_pid(30,0));
 }
